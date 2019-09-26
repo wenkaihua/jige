927a6d5bbd058fc66c3ff424e5b51cd010ea8849
wenkaihua <453012910@qq.com>
2019-09-26 18:59:03 +0800

    内民

---
+++ src/main/java/com/example/neiming/NeimingApplication.java
@@ -0,0 +1,13 @@
+package com.example.neiming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeimingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeimingApplication.class, args);
+    }
+
+}
